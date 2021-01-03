bf958b39e7d154ea7fce71cc843707f66ffd8253
gunavel <gunavel.bharathi@gmail.com>
2021-01-03 20:37:07 +0530

    Update setup-crowdin-project.sh

--- .github/setup-crowdin-project.sh
+++ .github/setup-crowdin-project.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e # Exit immediately if any scripts returns non zero status
 
+CROWDIN_API_TOKEN=$1
 
 echo "GITHUB_REPOSITORY=$GITHUB_REPOSITORY"
 repo_name=$(echo "${GITHUB_REPOSITORY}" | cut -d'/' -f 2-)
 echo "repo_name=$repo_name"
-resp=$(curl -XGET -H 'Authorization: Bearer ${{ secrets.TOK }}' -H "Content-type: application/json" 'https://api.crowdin.com/api/v2/projects')
+resp=$(curl -XGET -H 'Authorization: Bearer "$CROWDIN_API_TOKEN"' -H "Content-type: application/json" "https://api.crowdin.com/api/v2/projects")
 echo "resp=$resp"
