2989aaf1977af50b0dd7656b48acb51dfeedd745
Gunavel <gunavel.bharathi@gmail.com>
2021-01-04 19:01:44 +0530

    test github api

---
+++ .github/node-app/run.js
@@ -0,0 +1,26 @@
+/**
+ * Run [command] on the given [configFile] on all languages in the given [langDir]
+ *
+ * ```
+ * runAll [command] [configfile] [langDir]
+ * ```
+ */
+const fs = require("fs");
+const Promise = require("bluebird");
+const shell = Promise.promisifyAll(require("shelljs"));
+
+const [configFile, appsDir] = ["config.json", "apps"];
+const appFiles = fs.readdirSync(appsDir);
+
+// Make the repo directory now so that child processes don't error out
+if (shell.ls("repo").code !== 0) {
+  shell.mkdir("repo");
+}
+
+// We run the script separately for each language so that the shelljs global state
+// (e.g. working directory) doesn't interfere between runs
+Promise.map(appFiles, (appFile) => {
+  const path = `${appsDir}/${appFile}`;
+  console.log("path:", path);
+  return shell.execAsync(`node api.js ${configFile} ${path}`);
+});
